e3f476e720a8c2381ed89132838610437352a818
dance2die <dontbothermewithspam@gmail.com>
2016-03-27 09:42:23 -0400

    Question and answeres are randomly generated

--- app/src/main/java/com/dance2die/demoandroidudemybraintrainer/MainActivity.java
+++ app/src/main/java/com/dance2die/demoandroidudemybraintrainer/MainActivity.java
@@ -11,11 +11,14 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button startButton;
+    private ArrayList<Integer> answers = new ArrayList<>();
+    private int locationOfCorrectAnswer;
 
     public void start(View view){
         startButton.setVisibility(View.INVISIBLE);
@@ -42,9 +45,35 @@ public class MainActivity extends AppCompatActivity {
         Random random = new Random();
         int a = random.nextInt(21);
         int b = random.nextInt(21);
+        int answer = a + b;
 
         TextView sumTextView = (TextView) findViewById(R.id.sumTextView);
         sumTextView.setText(String.format("%s + %s", Integer.toString(a), Integer.toString(b)));
+
+        locationOfCorrectAnswer = random.nextInt(4);
+        int incorrectAnswer;
+
+        for(int i = 0; i < 4; i++){
+            if (i == locationOfCorrectAnswer){
+                answers.add(answer);
+            } else {
+                do {
+                    incorrectAnswer = random.nextInt(41);
+                } while (incorrectAnswer == answer);
+
+                answers.add(incorrectAnswer);
+            }
+        }
+
+        Button button0 = (Button) findViewById(R.id.button0);
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
     }
 
     @Override
